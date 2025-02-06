0db780145aef2f9d267fdde260820032ac5a413a
NuarLiveLOL <zaynkiller.12@gmail.com>
2025-02-06 20:40:13 +0500

    SHABLON_bot

--- bot.py
+++ bot.py
@@ -4,23 +4,34 @@ from aiogram.types import Message
 from aiogram.filters import Command
 
 TOKEN = '8043275462:AAH7nY9QOojLTutdI_yIs7fn6G_H-gmLsmA'
+ADMIN_USER_ID = 5492942922  # Укажите ваш ID пользователя
 
 # Создаем бота и диспетчер
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Проверка, является ли пользователь администратором
+async def is_admin(message: Message) -> bool:
+    return message.from_user.id == ADMIN_USER_ID
+
 # Команда /start
 @dp.message(Command("start"))
 async def start(message: Message):
-    await message.reply("Привет! Используйте /connect, чтобы подключиться.")
+    if await is_admin(message):
+        await message.reply("Привет! Используйте /connect, чтобы подключиться.")
+    else:
+        await message.reply("У вас нет прав для использования этой команды.")
 
 # Команда /stop для завершения работы бота
 @dp.message(Command("stop"))
 async def stop(message: Message):
-    await message.reply("Бот завершает свою работу...")
-    # Останавливаем polling и закрываем соединение с ботом
-    await dp.stop_polling()
-    await bot.close()
+    if await is_admin(message):
+        await message.reply("Бот завершает свою работу...")
+        # Останавливаем polling и закрываем соединение с ботом
+        await dp.stop_polling()
+        await bot.close()
+    else:
+        await message.reply("У вас нет прав для завершения работы бота.")
 
 # Запуск бота
 async def main():
@@ -28,6 +39,7 @@ async def main():
     try:
         # Запускаем polling
         await dp.start_polling(bot)
+        
     except asyncio.CancelledError:
         pass
     finally:
